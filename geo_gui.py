87a4f2879a531174d1231b44449b433809f967c1
Giulia <giulia.calanca@simplyopen.org>
2019-03-11 15:08:28 +0100

    buttons grid gui

--- geo_gui.py
+++ geo_gui.py
@@ -1,14 +1,55 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import wx
 
 
-class MyFrame(wx.Frame):
+class Example(wx.Frame):
 
-    def __init__(self, parent, title):
-        wx.Frame.__init__(self, parent, size(200,100), title=title)
-        self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
-        self.Show(True)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.InitUI()
 
-app = wx.App(False)
-frame = MyFrame(None, "Small Editor")
-app.MainLoop()
+    def InitUI(self):
+        # menubar setup
+        menubar = wx.MenuBar()
+        fileMenu = wx.Menu()
+        fileItem = fileMenu.Append(wx.ID_EXIT, 'Quit', 'Quit application')
+        menubar.Append(fileMenu, '&File')
+        self.SetMenuBar(menubar)
+        self.Bind(wx.EVT_MENU, self.OnQuit, fileItem)
+
+        # panel setup
+        pnl = wx.Panel(self)
+        grid = wx.GridSizer(2)
+
+        grid.AddMany([
+            (wx.Button(pnl, wx.ID_CANCEL), 0, wx.TOP | wx.LEFT, 9),
+            (wx.Button(pnl, wx.ID_DELETE), 0, wx.TOP, 9),
+            (wx.Button(pnl, wx.ID_SAVE), 0, wx.LEFT, 9),
+            (wx.Button(pnl, wx.ID_EXIT)),
+            (wx.Button(pnl, wx.ID_STOP), 0, wx.LEFT, 9),
+            (wx.Button(pnl, wx.ID_NEW))
+        ])
+        self.Bind(wx.EVT_BUTTON, self.OnQuit, id=wx.ID_EXIT)
+
+        pnl.SetSizer(grid)
+
+        # self.SetSize((400, 300))
+        self.SetTitle('Standard ids')
+        self.Centre()
+
+    def OnQuit(self, e):
+        self.Close()
+
+
+def main():
+
+    app = wx.App()
+    ex = Example(None)
+    ex.Show()
+    app.MainLoop()
+
+
+if __name__ == '__main__':
+    main()
