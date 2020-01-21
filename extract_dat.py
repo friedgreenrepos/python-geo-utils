3be4c57cace0374fa6a7dffa5593ddb09a282bdc
Giulia <giuli4994@gmail.com>
2020-01-21 17:42:42 +0100

    output is randomly ordered for some reason

--- extract_dat.py
+++ extract_dat.py
@@ -1,7 +1,34 @@
-with open('data_samples/extract_from_test.txt') as input:
-    with open('data_samples/extract_to_test.dat') as output:
-        for line in input:
-            if not line.strip():
-                continue
-            elif line[2:15] == "Nome Stazione":
-                n_staz = line[17:].strip()
+def represents_int(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+
+with open('data_samples/extract_from.txt', 'r') as input:
+    # extract_lst = []
+    for line in input:
+        if not line.strip():
+            continue
+        if line[2:15] == "Nome Stazione":
+            # mentre nome stazione è sempre uguale (speriamo)
+            # n_staz potrebbe non avere sempre 3 cifre
+            n_staz = line[17:].strip()
+            staz = [line[2:15], n_staz]
+            # output.write("1")
+            # output.write("|")
+            # output.write(n_staz)
+            # output.write("|")
+            # output.write('\n')
+            extract_lst.append(staz)
+        elif represents_int(line[24:25]):
+            info = line[24:117].split()
+            # output.write(info[0])
+            # output.write("|")
+            # output.write(info[4])
+            # output.write("|")
+            # output.write(info[5])
+            # output.write("|")
+            # output.write('\n')
+            extract_lst.append(coords)
